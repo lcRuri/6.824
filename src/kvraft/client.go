24224077e41c13b79444334f969fef4643af67ac
lcRuri <1793185558@qq.com>
2023-02-19 20:19:50 +0800

    lab3A 增加了client对server发送rpc请求的部分代码

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,13 +1,21 @@
 package kvraft
 
-import "6.824/labrpc"
+import (
+	"6.824/labrpc"
+	"sync"
+	"sync/atomic"
+	"time"
+)
 import "crypto/rand"
 import "math/big"
-
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	mu       sync.Mutex
+	clientId int64
+	seqId    int64
+	leaderId int
 }
 
 func nrand() int64 {
@@ -21,6 +29,7 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+	ck.clientId = nrand()
 	return ck
 }
 
@@ -35,11 +44,38 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // the types of args and reply (including whether they are pointers)
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
-//
-func (ck *Clerk) Get(key string) string {
+//获取键的当前值。
+//如果键不存在，则返回 “”。
+//面对所有其他错误，不断尝试。
+//您可以使用如下代码发送 RPC：
+//ok ：= ck.servers[i].call（“KVServer.Get”， &args， &reply）
 
-	// You will have to modify this function.
-	return ""
+//参数和回复的类型（包括它们是否是指针）
+//必须与 RPC 处理程序函数的声明类型匹配
+//参数。并且回复必须作为指针传递。
+func (ck *Clerk) Get(key string) string {
+	args := GetArgs{
+		Key:      key,
+		ClientId: ck.clientId,
+		SeqId:    atomic.AddInt64(&ck.seqId, 1),
+	}
+
+	DPrintf("Client[%d] Get starts, Key=%s ", ck.clientId, key)
+	leaderId := ck.currentLeaderId()
+	for {
+		reply := GetReply{}
+		if ck.servers[leaderId].Call("KVServer.Get", &args, &reply) {
+			if reply.Err == OK {
+				return reply.Value
+			} else if reply.Err == ErrNoKey {
+				return ""
+			}
+		}
+
+		leaderId = ck.changeLeaderId()
+		time.Sleep(1 * time.Millisecond)
+	}
+
 }
 
 //
@@ -51,11 +87,58 @@ func (ck *Clerk) Get(key string) string {
 // the types of args and reply (including whether they are pointers)
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
-//
+////
+//由放置和追加共享。
+////
+//您可以使用如下代码发送 RPC：
+//ok ：= ck.servers[i].Call（“KVServer.PutAppend”， &args， &reply）
+////
+//参数和回复的类型（包括它们是否是指针）
+//必须与 RPC 处理程序函数的声明类型匹配
+//参数。并且回复必须作为指针传递。
+///
+
+//PutAppend 先找到leader 在将key-value信息发送给leader
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	args := PutAppendArgs{
+		Key:      key,
+		Value:    value,
+		Op:       op,
+		ClientId: ck.clientId,
+		SeqId:    atomic.AddInt64(&ck.seqId, 1),
+	}
+
+	DPrintf("Client[%d] PutAppend, Key=%s Value=%s", ck.clientId, key, value)
+
+	leaderId := ck.currentLeaderId()
+
+	for {
+		reply := PutAppendReply{}
+		if ck.servers[leaderId].Call("KVServer.PutAppend", &args, &reply) {
+			if reply.Err == OK {
+				break
+			}
+		}
+
+		leaderId = ck.changeLeaderId()
+		time.Sleep(1 * time.Millisecond)
+	}
 }
 
+func (ck *Clerk) currentLeaderId() (leaderId int) {
+	ck.mu.Lock()
+	defer ck.mu.Unlock()
+	leaderId = ck.leaderId
+	return
+}
+
+func (ck *Clerk) changeLeaderId() int {
+	ck.mu.Lock()
+	defer ck.mu.Unlock()
+	ck.leaderId = (ck.leaderId + 1) % len(ck.servers)
+	return ck.leaderId
+}
 func (ck *Clerk) Put(key string, value string) {
 	ck.PutAppend(key, value, "Put")
 }
